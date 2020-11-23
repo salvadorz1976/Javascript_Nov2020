73958db19aca4f7cfcffc7b8f2c94e8afd288f25
Salvador Sandoval <salvador.sandoval@protonmail.com>
2020-11-22 18:03:16 -0600

    actualizacion practica

--- Operadores.js
+++ Operadores.js
@@ -6,4 +6,13 @@ console.log("resultado: ", a + b)
 console.log("resultado: ", c - b)
 console.log("resultado: ", a * b)
 console.log("resultado: ", 98 % 2)
+console.log("resultado: ", ++a )
+console.log("resultado: ", a )
+console.log("resultado: ", --c)
 
+console.log("cadena", 879 , "3")
+console.log("Canena1 " + "Cadena 2")
+
+const cinco = 5
+const diez = 10
+console.log("Quince es " + (cinco + diez) + ' y  no ' + (2 * diez + 5) +'.')
