addc3e1bde16138b6a18a81d2e2b5ac78f9789d8
Salvador Sandoval <salvador.sandoval@protonmail.com>
2020-11-23 08:35:03 -0600

    Apuntes sobre condicionantes

---
+++ condicionantes.js
@@ -0,0 +1,16 @@
+var texto;
+texto = "Servidor on"
+
+
+if (texto == 'Servidor Off') {
+    console.log('Test pass')
+}
+else {
+    console.log("Text fail")
+}
+
+var num = 48
+if (num == 50){
+    console.log("Valor: ", num)
+    console.log("Numero menor")
+}
