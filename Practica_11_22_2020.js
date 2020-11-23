cc0d6287cb767278b5289e83f5ed3780eda8b80b
Salvador Sandoval <salvador.sandoval@protonmail.com>
2020-11-23 08:27:53 -0600

    Practia tarae del 11_22

---
+++ Practica_11_22_2020.js
@@ -0,0 +1,24 @@
+/* Tarea 
+Ejercicio
+En este ejercicio, debe construir un if dentro de la instrucción 
+de la función checkNumber que verifica si el número myNumber 
+es igual a 42. 
+Si ese es el caso, la función debe imprimir usando console.log 
+la palabra 'correcta'. 
+Si myNumber no es igual a 42, la función debe imprimirse 
+usando console.log la palabra incorrecta. 
+Puede nombrar el argumento pasado a una función 
+proporcionando el nombre entre paréntesis. 
+Por ejemplo, función myFunction (myArgument).
+*/
+
+function myFunction (myArgument1, myArgument2){
+    return (myArgument1 + myArgument2)
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+
+console.log(isNumber(5,8))
